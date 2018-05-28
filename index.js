78df9e9a8eb3bcdbfe2e6cc42b53afe342d54dc2
Yisroel Weisberg <weisbeym@mail.uc.edu>
2018-05-28 00:50:25 -0400

    basic DOM examples cont. more

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 
 // Make a variable referencing a HTML element
 const helloHeader = document.getElementById("helloHeader");
+
 // gets all elements with this tag
 const helloParagraph = document.getElementsByTagName("p");
 
 // change that element, in this case making the txt red
 helloHeader.style.color ="red";
 
-// console.log(helloParagraph);
 
 // creates a new element, is not automatically placed in the document
 const testParagraph = document.createElement("p");
@@ -18,9 +18,12 @@ const testParagraph = document.createElement("p");
 //testParagraph.innerText = '<span style="color: blue">Hello</span>'
 
 //this version adds HTML inside the parent element, so it will pick up the spans this time
-testParagraph.innerHTML = '<span style="color: blue">Hello</span>'
+//testParagraph.innerHTML = '<span style="color: blue">Hello</span>'
 
 // this places a child element into the document body, in this case the paragraph we made above
 document.body.appendChild(testParagraph);
+
+const helloTextNode = document.createTextNode("Hello");
+testParagraph.appendChild(helloTextNode);
 
 console.log(testParagraph);
