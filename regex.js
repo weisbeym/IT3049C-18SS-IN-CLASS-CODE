df4a7ca5c12919de576982c1c887b0d1afd1ff66
Yisroel Weisberg <weisbeym@mail.uc.edu>
2018-06-03 11:46:52 -0400

    setting up environment for JSON lecture.

--- regex.js
+++
@@ -1,6 +0,0 @@
-const literalRegex = /[a,e,i,o,u]/gi;
-const constructorRegex = RegExp("[a,e,i,o,u]", "gi")
-
-const names = "Name1, Name2, Name3"
-
-const namesArray = names.split(", ") // [ "Name1", "Name2", "Name3" ]
